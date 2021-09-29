6029383fc8de9b42ab30091a5ade93273240acbb
versedpro <ultraj0330@gmail.com>
2021-09-29 10:37:45 +0800

    chore: Update presale concept

--- contracts/PreSale.sol
+++ contracts/PreSale.sol
@@ -50,11 +50,14 @@ contract Presale is Ownable, ReentrancyGuard {
     uint256 public constant oneMonth = 30 days;
     uint256 public constant unlockPerMonth = 20;
     uint256 public startTimestamp;
-    uint256 public softCapAmount = 50000000000000000000000; // 50k busd
-    uint256 public hardCapAmount = 200000000000000000000000; // 200k busd
-    uint256 public tokenPrice = 500000000000000000; //0.5 busd
-    // uint256 public hardCapPrice = 1000000000000000000; //1 busd
-    uint256 public maxBusdPerWallet = 1000000000000000000000; //1k busd
+    uint256 public softCapAmount = 200000000000000000000000; // 200k USD
+    uint256 public hardCapAmount = 500000000000000000000000; // 500k USD
+    uint256 public fistTokenPrice = 200000000000000000; // 0.2 BUSD
+    uint256 public secondTokenPrice = 300000000000000000; // 0.3 BUSD
+    uint256 public tokenPrice = fistTokenPrice; // First Stage: 0.2 BUSD
+    // uint256 public hardCapPrice = 1000000000000000000; //1 BUSD
+    uint256 public maxBusdPerWallet = 25000000000000000000000; // 25k BUSD
+    uint256 public maxSupply = 80000000000000000000000; // 80k CRSS
 
     constructor(
         IBEP20 _crssToken,
@@ -63,7 +66,7 @@ contract Presale is Ownable, ReentrancyGuard {
         uint256 _startTimestamp
     ) public {
         require(address(_crssToken) != address(0), "Presale: Token address should not be zero address");
-        require(address(_busd) != address(0), "Presale: busd token address should not be zero address");
+        require(address(_busd) != address(0), "Presale: BUSD token address should not be zero address");
         require(_masterWallet != address(0), "Presale: master wallet address should not be zero address");
         require(_startTimestamp >= _getNow(), "Presale: Presale start time invalid");
 
@@ -112,7 +115,12 @@ contract Presale is Ownable, ReentrancyGuard {
 
         require(user.depositAmount + _amount <= maxBusdPerWallet, "Presale.deposit: deposit amount is bigger than max deposit amount");
 
+        if (totalDepositedBusdBalance >= softCapAmount) {
+            tokenPrice = secondTokenPrice;
+        }
+
         uint256 rewardTokenAmount = _amount.mul(1e18).div(tokenPrice);
+        require(totalRewardAmount.add(rewardTokenAmount) > maxSupply, "Presale.deposit: The desired amount must be less than the total presale amount");
         require(crssToken.balanceOf(address(this)).add(totalWithdrawedAmount).sub(totalRewardAmount) >= rewardTokenAmount, "Presale.deposit: not enough token to reward" );
         if(user.depositAmount == 0) {
             investors.push(msg.sender);
