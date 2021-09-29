e073e7b8554fe447cd22f911605a19c066cbcb53
lifeisgooddev <lifeisgood91028@gmail.com>
2021-09-29 05:00:55 -0400

    fix softcap check case in presale

--- migrations/5_presale_migration.js
+++ migrations/5_presale_migration.js
@@ -2,5 +2,5 @@ const Presale = artifacts.require("Presale");
 
 module.exports = async function(deployer) {
   const block = await web3.eth.getBlock("latest");
-  deployer.deploy(Presale, "0xA98D21C3D61A7EB9Dd3BE9C9a1132Abb7c7Be2Dd", "0xeD24FC36d5Ee211Ea25A80239Fb8C4Cfd80f12Ee", "0x6973a5D5e2Bd3bBDe498104FeCDF3132A3c545aB", block.number);
+  await deployer.deploy(Presale, "0xA98D21C3D61A7EB9Dd3BE9C9a1132Abb7c7Be2Dd", "0xeD24FC36d5Ee211Ea25A80239Fb8C4Cfd80f12Ee", "0x6973a5D5e2Bd3bBDe498104FeCDF3132A3c545aB", block.number);
 };
