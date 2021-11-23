2f9da8eccb421a7cff1988bc408af22b43b54ebf
versedpro <ultraj0330@gmail.com>
2021-11-23 16:44:06 +0800

    chore: Update migration of presale V2

---
+++ migrations/7_presalev2_migration.js
@@ -0,0 +1,6 @@
+const Presale = artifacts.require("PresaleV2");
+
+module.exports = async function(deployer) {
+  const block = await web3.eth.getBlock("latest");
+  await deployer.deploy(Presale, "0xA98D21C3D61A7EB9Dd3BE9C9a1132Abb7c7Be2Dd", "0xeD24FC36d5Ee211Ea25A80239Fb8C4Cfd80f12Ee", "0x6973a5D5e2Bd3bBDe498104FeCDF3132A3c545aB", block.timestamp + 20);
+};
